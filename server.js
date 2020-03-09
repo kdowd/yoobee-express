42df54b435b81187c95d8de5a76ba0276e80ce27
frappe76 <kevin.dowd@acgedu.com>
2020-03-09 21:45:25 +1300

    monday update3

--- server.js
+++ server.js
@@ -132,6 +132,6 @@ app.get("/*", (req, res) => {
 
 // and finally,  lets listen
 const port = 4000;
-app.listen(process.env.port || port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
