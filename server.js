c321bbc5c7741eaa1e2e597911e408537bec5b05
kdowd <kevin.dowd@gmail.com>
2020-03-12 21:19:14 +1300

    gfggfg

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const myconn = require("./connection");
 // every single collection will need a model ,,,,
-//hmmmmm, **********************************************;
+//hmmmmm, ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^;
 
 const app = express();
 app.use(cors());
